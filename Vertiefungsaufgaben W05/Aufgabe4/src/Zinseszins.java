08efbe5cc6217523e19ea5a4ab387ae624d0755c
paseg <pascal.eggenschwiler@students.fhnw.ch>
2026-03-28 11:07:21 +0100

    Augabe4 gemacht

---
+++ Vertiefungsaufgaben W05/Aufgabe4/src/Zinseszins.java
@@ -0,0 +1,26 @@
+public class Zinseszins {
+	public static void main(String[] args) {
+
+		String anfangskontostand = IO.readln("Anfangskontostand?");
+		double aktuellerKontostand = Double.parseDouble(anfangskontostand);
+
+		String zinssatzString = IO.readln("Zinssatz in %?");
+		double zinssatz = Double.parseDouble(zinssatzString) / 100;
+
+		String anzahlJahreString = IO.readln("Anzahl Jahre?");
+		int anzahlJahre = Integer.parseInt(anzahlJahreString);
+
+		int aktuellerKontostandFr = 0;
+		int aktuellerKontostandRp = 0;
+
+		for (int i = 1; i <= anzahlJahre; i++) {
+			aktuellerKontostand += (aktuellerKontostand * zinssatz);
+                        aktuellerKontostandFr = (int) aktuellerKontostand;
+			aktuellerKontostandRp = (int) (aktuellerKontostand * 100) % 100;
+			IO.println("Der Kontostand nach " + i + " Jahren beträgt: " + aktuellerKontostandFr + "." + aktuellerKontostandRp + " CHF");
+		}
+		if (aktuellerKontostand < 0) {
+			IO.println("Der Kontostand muss midestens 0 CHF betragen");
+		}
+	}
+}
