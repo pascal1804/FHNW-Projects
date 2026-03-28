1cbd08a5d64f2298e8ab86c87f215438c03bbf6a
paseg <pascal.eggenschwiler@students.fhnw.ch>
2026-03-28 15:31:30 +0100

    Aufgabe 8 gemacht

---
+++ Vertiefungsaufgaben W05/Aufgabe8/src/TurtleOnSteroids.java
@@ -0,0 +1,32 @@
+import ch.trick17.turtle4j.Turtle;
+
+public class TurtleOnSteroids {
+	public void main() {
+		Turtle.setSpeed(2147483647);
+		double penWidth = 1;
+		double strecke = 10;
+		int winkel = 121;
+		int farbton = 182;
+		for (int j = 1; j <= 5; j++) {
+			for (int k = 1; k <= 6; k++) {
+				for (int i = 1; i <= 12; i++) {
+					Turtle.setPenColor("hsl(" + farbton + ",100%,100%)");
+					Turtle.setPenWidth(penWidth);
+					Turtle.forward(strecke);
+					Turtle.right(winkel);
+					Turtle.forward(strecke);
+					Turtle.left(winkel);
+					strecke += 0.4;
+					penWidth += 0.01;
+					farbton++;
+					if (farbton > 300) {
+						farbton = 180;
+					}
+					Turtle.right(61);
+				}
+			}
+			Turtle.forward(5);
+			Turtle.left(10);
+		}
+	}
+}
